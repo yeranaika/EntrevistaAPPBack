0064ff99c6ea62e356223f1d996c59fc878de1da
yeranaika <darca604@gmail.com>
2025-11-13 20:11:25 -0300

    como te odio lara

--- src/main/kotlin/routes/auth/authRoutes.kt
+++ src/main/kotlin/routes/auth/authRoutes.kt
@@ -1,12 +1,9 @@
 package routes.auth
 
 import com.auth0.jwt.algorithms.Algorithm
-import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
-/**
- * Mantiene la ruta base /auth y monta subrutas.
- */
+// Mantiene la ruta base /auth y monta las subrutas
 fun Route.authRoutes(
     issuer: String,
     audience: String,
